d774ffcdcf85da4a4ccb6e71b2ab026e2a5f0428
SamuelSanta <samuelsanta2004@gmail.com>
2023-04-03 12:53:36 -0500

    se agrego la informacion en nosotros, se necesitan ajustes

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -9,51 +9,57 @@ import { Footer } from "@/widgets/layout";
 export function Profile() {
   return (
     <>
-      <section className="relative block h-[250vh]">
+      <section className="relative block h-[270vh]">
         <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/local_todos.JPG')] bg-cover bg-center" />
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
         <div className="absolute top-20 left-0 w-full h-full flex flex-col justify-start items-center">
           <div className="text-white text-8xl font-roulette">Sobre Nosotros...</div>
           <div className="text-white text-4xl font-roulette">#Brunettelovers</div>
         </div>
-        
+
 
         <div className=" sm:py-48">
           <div className="mx-auto max-w-6xl px-6 lg:px-10">
             <div className="mx-auto mt-16">
               <div className="grid  grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-10 ">
-                <div className="relative">
-                  <div className="h-1/2">
-                    <img src="/img/team1.jpg" width="80%"  />
-                  </div>
+                <div className="relative flex flex-col justify-center items-center">
+                  <img src="/img/team1.jpg" width="50%" />
                 </div>
 
-                <div className="relative">
-                  <div className="mt-2 font-lemon leading-7 text-gray-300">Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar ut turpis imperdiet tristique. Curabitur commodo urna in sapien consequat commodo non vitae arcu. Pellentesque ac lacus leo. Sed volutpat hendrerit nisl sit amet fringilla. Nam sed tellus non massa commodo euismod eu vitae eros. Phasellus iaculis eget elit at cursus. Proin suscipit at quam sit amet ultrices. Aliquam non arcu bibendum, auctor velit gravida, tincidunt nulla. Aenean eu sapien id metus congue tempus non id ipsum. Ut ornare, quam a lacinia volutpat, diam nunc maximus purus, ac ultricies eros nulla a diam. Cras efficitur orci in leo consequat ultricies. Nullam ornare ex enim, eu dictum massa pellentesque vel. Aenean ut dui dolor. In at tincidunt urna. Pellentesque suscipit id risus eget sollicitudin. Nullam et nulla quis est interdum aliquet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar ut turpis imperdiet tristique. Curabitur commodo urna in sapien consequat commodo non vitae arcu. Pellentesque ac lacus leo. Sed volutpat hendrerit nisl sit amet fringilla. Nam sed tellus non massa commodo euismod eu vitae eros. Phasellus iaculis eget elit at cursus. Proin suscipit at quam sit amet ultrices. Aliquam non arcu bibendum, auctor velit gravida, tincidunt nulla. Aenean eu sapien id metus congue tempus non id ipsum. Ut ornare, quam a lacinia volutpat, diam nunc maximus purus, ac ultricies eros nulla a diam. Cras efficitur orci in leo consequat ultricies. Nullam ornare ex enim, eu dictum massa pellentesque vel. Aenean ut dui dolor. In at tincidunt urna. Pellentesque suscipit id risus eget sollicitudin. Nullam et nulla quis est interdum aliquet.</div>
+                <div className="relative flex flex-col justify-center items-center">
+                  <div className="mt-2 font-lemon leading-7 text-gray-300">Desde 2018, dejando el corazón en cada producto.Nuestra misión es entregarlo todo, ofrecer lo mejor, porque cada detalle merece ser pensado, cada sueño merece ser cumplido, por eso perseveramos.Poco a poco vamos construyendo nuestra historia.Gracias por acompañarnos en el camino.</div>
                 </div>
 
-                <div className="relative">
-                  <div className="mt-2 font-lemon leading-7 text-gray-300">Lorem2 ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar ut turpis imperdiet tristique. Curabitur commodo urna in sapien consequat commodo non vitae arcu. Pellentesque ac lacus leo. Sed volutpat hendrerit nisl sit amet fringilla. Nam sed tellus non massa commodo euismod eu vitae eros. Phasellus iaculis eget elit at cursus. Proin suscipit at quam sit amet ultrices. Aliquam non arcu bibendum, auctor velit gravida, tincidunt nulla. Aenean eu sapien id metus congue tempus non id ipsum. Ut ornare, quam a lacinia volutpat, diam nunc maximus purus, ac ultricies eros nulla a diam. Cras efficitur orci in leo consequat ultricies. Nullam ornare ex enim, eu dictum massa pellentesque vel. Aenean ut dui dolor. In at tincidunt urna. Pellentesque suscipit id risus eget sollicitudin. Nullam et nulla quis est interdum aliquet.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar ut turpis imperdiet tristique. Curabitur commodo urna in sapien consequat commodo non vitae arcu. Pellentesque ac lacus leo. Sed volutpat hendrerit nisl sit amet fringilla. Nam sed tellus non massa commodo euismod eu vitae eros. Phasellus iaculis eget elit at cursus. Proin suscipit at quam sit amet ultrices. Aliquam non arcu bibendum, auctor velit gravida, tincidunt nulla. Aenean eu sapien id metus congue tempus non id ipsum. Ut ornare, quam a lacinia volutpat, diam nunc maximus purus, ac ultricies eros nulla a diam. Cras efficitur orci in leo consequat ultricies. Nullam ornare ex enim, eu dictum massa pellentesque vel. Aenean ut dui dolor. In at tincidunt urna. Pellentesque suscipit id risus eget sollicitudin. Nullam et nulla quis est interdum aliquet.</div>
+                <div className="relative flex flex-col justify-center items-center ">
+                  <div className="mt-2 font-lemon leading-7 text-gray-300">Participamos activamente en lasmejores ferias, de valledupar,destacándonos por nuestra forma dinámica de servirlea nuestros clientes,lo que nos ha permitido crecerde manera exponencial en nuestras plataformas digitales.Hoy contamos con una comunidad  de más de 16.000 seguidores.</div>
                 </div>
 
-                <div className="relative">
-                <div className="h-1/2">
-                    <img src="/img/local_todos.jpg" width="80%" />
-                  </div>
+                <div className="relative flex flex-col justify-center items-center ">
+                  <img src="/img/local_todos.jpg" width="50%" />
+                </div>
+
+
+                <div className="relative flex flex-col justify-center items-center">
+                  <img src="/img/clasico_cereza.JPG" width="50%" />
+                </div>
+
+                <div className="relative flex flex-col justify-center items-center">
+                  <div className="mt-2 font-roulette text-4xl leading-7 text-gray-300">¿QUE HACEMOS?</div>
+                  <div className="mt-2 font-lemon leading-7 text-gray-300">Brunette Artesanal, es reconocido por elaborar un yogurt gourmet, que tiene como característicasespeciales su sabor, consistencia, textura suave al paladar y su versatilidadpara ser combinado con diferentes topping, siendo pioneros en esta manera diferente de consumir yogur.</div>
+                </div>
+
+                <div className="relative flex flex-col justify-center items-center">
+                  <div className="mt-2 font-roulette text-4xl leading-7 text-gray-300">¡NUESTROS PRODUCTOS!</div>
+                  <div className="mt-2 font-lemon leading-7 text-gray-300">Nuestra estrella, el yogur natural brunette... el que con todo combina, de el se desprenden los diferentes productos que ofrecemos a nuestro público! teniendo opciones como “ARMA TU BRUNETTE” que amplia las posibilidades a nuevas experiencias.</div>
+                </div>
+
+                <div className="relative flex flex-col justify-center items-center">
+                  <img src="/img/productos.JPG" width="30%" />
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-
-      <section className="relative h-[2vh]">
-        <div className=" text-4xl font-roulette">#Brunettelovers</div>
-      </section>
-
-      <section className="relative h-[100vh]">
-        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/local_todos.JPG')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
       </section>
 
       <div className="bg-custom-primary">
