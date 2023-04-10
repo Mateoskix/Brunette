a04d061be4267c3e9203105284b673972affe847
Mateo Gonzalez <mateogt0903@gmail.com>
2023-04-10 10:56:26 -0500

    implementado cerrar carrito en checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { Navbar } from "@/widgets/layout";
 import { ShoppingCart } from "@/widgets/layout";
 import routes from "@/routes";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-
   const [showCart, setShowCart] = useState(false);
+  const location = useLocation();
 
   const handleCartClick = () => {
     setShowCart(!showCart);
   };
 
+  useEffect(() => {
+    if (location.pathname === "/checkout") {
+      setShowCart(false);
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <div className="container static bg-white mx-auto">
-        <Navbar routes={routes} onCartClick={handleCartClick}/>
+        <Navbar routes={routes} onCartClick={handleCartClick} />
       </div>
       {showCart && <ShoppingCart />}
       <Routes>
