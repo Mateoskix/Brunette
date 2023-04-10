12209feb6ad95f8a810ddde744ac915a25886889
Mateo Gonzalez <mateogt0903@gmail.com>
2023-04-10 11:52:37 -0500

    modificación template carrito

--- src/widgets/layout/cart.jsx
+++ src/widgets/layout/cart.jsx
@@ -84,11 +84,11 @@ export function ShoppingCart() {
                   <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
                     <div className="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
                       <div className="flex items-start justify-between">
-                        <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
+                        <Dialog.Title className="text-lg font-bunya text-gray-900">Carrito</Dialog.Title>
                         <div className="ml-3 flex h-7 items-center">
                           <button
                             type="button"
-                            className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                            className="-m-2 p-2 text-custom-secundary hover:text-opacity-50"
                             onClick={() => setOpen(false)}
                           >
                             <span className="sr-only">Close panel</span>
@@ -102,7 +102,7 @@ export function ShoppingCart() {
                           <ul role="list" className="-my-6 divide-y divide-gray-200">
                             {products.map((product) => (
                               <li key={product.id} className="flex py-6">
-                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray">
                                   <img
                                     src={product.imageSrc}
                                     alt={product.imageAlt}
@@ -112,23 +112,23 @@ export function ShoppingCart() {
 
                                 <div className="ml-4 flex flex-1 flex-col">
                                   <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                    <div className="flex justify-between text-base font-lemon text-gray-900">
                                       <h3>
                                         <a href={product.href}>{product.name}</a>
                                       </h3>
                                       <p className="ml-4">{product.price}</p>
                                     </div>
-                                    <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                                    <p className="mt-1 font-bunya text-sm text-gray-500">{product.color}</p>
                                   </div>
-                                  <div className="flex flex-1 items-end justify-between text-sm">
-                                    <p className="text-gray-500">Qty {product.quantity}</p>
+                                  <div className="flex flex-1 items-end justify-between font-bunya text-sm">
+                                    <p className="text-gray-500">Cantidad {product.quantity}</p>
 
                                     <div className="flex">
                                       <button
                                         type="button"
-                                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                                        className="font-medium text-custom-secundary hover:text-opacity-50"
                                       >
-                                        Remove
+                                        Remover
                                       </button>
                                     </div>
                                   </div>
@@ -141,31 +141,31 @@ export function ShoppingCart() {
                     </div>
 
                     <div className="border-t border-gray-200 py-6 px-4 sm:px-6">
-                      <div className="flex justify-between text-base font-medium text-gray-900">
+                      <div className="flex justify-between font-bunya-bold text-lg text-gray-900">
                         <p>Subtotal</p>
                         <p>$262.00</p>
                       </div>
-                      <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                      <p className="mt-0.5 text-sm font-bunya text-gray-500">Costos de envío en el checkout.</p>
                       <div className="mt-6">
                         <div className="mt-6">
                           <Link
                             to="/checkout"
-                            className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                            className="flex items-center justify-center rounded-md border border-transparent bg-custom-secundary px-6 py-3 text-base font-bunya-bold text-white shadow-sm hover:bg-opacity-90"
                           >
-                            Checkout
+                            Ir al Checkout
                           </Link>
                         </div>
 
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <p>
-                          or &nbsp;
+                          O &nbsp;
                           <button
                             type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                            className="font-medium text-custom-secundary hover:opacity-50"
                             onClick={() => setOpen(false)}
                           >
-                            Continue Shopping
+                            Continúa Comprando
                             <span aria-hidden="true"> &rarr;</span>
                           </button>
                         </p>
