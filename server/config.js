d4d5fe39397f948509f51ddfa1de247a3c106558
Jennifer Torres <jennifer.torres@upb.edu.co>
2023-04-28 14:15:19 -0500

    Configuración bd

---
+++ server/config.js
@@ -0,0 +1,5 @@
+const config = {
+    connectionSQL: "Data Source=[localhost:3306];Initial Catalog=[Brunette];Persist Security Info=True;User ID=[Brunette];Password=[Brunette];"
+}
+export default config;
+module.exports = config;
