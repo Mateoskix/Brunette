4c2045bd1c00c5f677b47857476d73a927284adc
SamuelSanta <samuelsanta2004@gmail.com>
2023-05-16 00:10:19 -0500

    arreglo en ordenes vista admin

--- src/widgets/layout/item_order.jsx
+++ src/widgets/layout/item_order.jsx
@@ -2,58 +2,58 @@ import React, { useState } from "react";
 
 const products = [
     {
-      id: 1,
-      name: 'Throwback Hip Bag',
-      href: '#',
-      color: 'Salmon',
-      price: '$90.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-      imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
+        id: 1,
+        name: 'Throwback Hip Bag',
+        href: '#',
+        color: 'Salmon',
+        price: '$90.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
+        imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
     },
     {
-      id: 2,
-      name: 'Medium Stuff Satchel',
-      href: '#',
-      color: 'Blue',
-      price: '$32.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-      imageAlt:
-        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+        id: 2,
+        name: 'Medium Stuff Satchel',
+        href: '#',
+        color: 'Blue',
+        price: '$32.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt:
+            'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
     },
     {
-      id: 3,
-      name: 'Medium Stuff Satchel',
-      href: '#',
-      color: 'Blue',
-      price: '$32.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-      imageAlt:
-        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+        id: 3,
+        name: 'Medium Stuff Satchel',
+        href: '#',
+        color: 'Blue',
+        price: '$32.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt:
+            'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
     },
     {
-      id: 4,
-      name: 'Medium Stuff Satchel',
-      href: '#',
-      color: 'Blue',
-      price: '$32.00',
-      quantity: 1,
-      imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-      imageAlt:
-        'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
+        id: 4,
+        name: 'Medium Stuff Satchel',
+        href: '#',
+        color: 'Blue',
+        price: '$32.00',
+        quantity: 1,
+        imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
+        imageAlt:
+            'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
     },
     // More products...
-  ]
+]
 
 export function Ordeness(props) {
-    const {ordenes} = props;
+    const { ordenes } = props;
     console.log(ordenes);
     if (!Array.isArray(ordenes)) {
         // Si ordenes no es un array, haz la conversión necesaria.
         ordenes = [ordenes];
-      }
+    }
     return (
         <div className="bg-white">
             <div className="mx-20 ">
@@ -63,7 +63,7 @@ export function Ordeness(props) {
                 <div className="justify-between py-5">
                     <div className="flex flex-col sapce-y-4 ">
                         <div className="flex gap-x-4 py-2">
-                            <div className="min-w-0 flex-auto">
+                            <div className="min-w-0 flex-start">
                                 <p className="text-sm font-semibold leading-6 text-gray-900"></p>
                             </div>
                             <div className="min-w-0 flex-auto">
@@ -87,35 +87,35 @@ export function Ordeness(props) {
                             <div className="hidden sm:flex sm:flex-col sm:items-center">
                                 <p className="text-sm font-semibold leading-6 text-gray-900">Estado</p>
                             </div>
-                            
+
                         </div>
                         {ordenes.map((orden) => (
-                            <div className="flex py-2 border-t border-black ">    
-                                <div className="min-w-0 flex-auto">
+                            <div className="flex py-2 border-t border-black ">
+                                <div className="min-w-0 flex-inital">
                                     <p className="text-sm leading-6 text-gray-900"></p>
                                 </div>
-                                <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">Producto 1, Producto 2</p>
+                                <div className="min-w-0 flex-inital">
+                                    <p className="text-sm w-32 leading-6 text-gray-900">{orden.productos}</p>
                                 </div>
-                                <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">Alejandro Gaviria Perez</p>
-                                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">correo@example.com</p>
+                                <div className="min-w-0 flex-auto pl-10">
+                                    <p className="text-sm w-36 leading-6 text-gray-900">{orden.nombre_cliente}</p>
+                                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{orden.correo}</p>
                                     <p className="mt-1 truncate text-xs leading-5 text-gray-500">3002691290</p>
                                 </div>
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">carrea 38 #84-56 Don Francisco</p>
+                                    <p className="text-sm w-24 leading-6 text-gray-900">{orden.direccion}</p>
                                 </div>
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">NO QUIERO FRESAS</p>
+                                    <p className="text-sm w-20 leading-6 text-gray-900">{orden.nota}</p>
                                 </div>
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">2023-05-12 12:14:22</p>
+                                    <p className="text-sm w-16 leading-6 text-gray-900">{orden.fecha}</p>
                                 </div>
                                 <div className="min-w-0 flex-auto">
-                                    <p className="text-sm leading-6 text-gray-900">$85.000</p>
+                                    <p className="text-sm leading-6 text-gray-900">{orden.valor}</p>
                                 </div>
-                                <div className="hidden sm:flex sm:flex-col sm:items-center">
-                                <p className="text-sm font-semibold leading-6 text-green-500">Pendiente</p>
+                                <div className="flex-end">
+                                    <p className="text-sm font-semibold leading-6 text-green-500">{orden.estado}</p>
                                 </div>
                             </div>
 
