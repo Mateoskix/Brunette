63e33834f9e05b210de221c493b59a1b94466a4b
SamuelSanta <samuelsanta2004@gmail.com>
2023-05-15 22:35:55 -0500

    arreglos en item-proc

--- src/widgets/layout/item_proc.jsx
+++ src/widgets/layout/item_proc.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import numeral from 'numeral';
-import {BiTrash, BiEditAlt} from 'react-icons/bi';
+import { BiTrash, BiEditAlt } from 'react-icons/bi';
 
 
 export function Prods(props) {
@@ -24,14 +24,14 @@ export function Prods(props) {
                 {state === "productos" && (
                     <div className="justify-between py-5">
                         <h2 className="text-2xl font-bold text-gray-900">
-                    PRODUCTOS <button onClick={handleAgregarClick} className="ml-10 hover:bg-custom-primary text-base border-2 px-4 py-2 rounded-lg">Agregar</button>
-                </h2>
+                            PRODUCTOS <button onClick={handleAgregarClick} className="ml-10 hover:bg-custom-primary text-base border-2 px-4 py-2 rounded-lg">Agregar</button>
+                        </h2>
                         <div className="flex flex-col sapce-y-4 ">
                             <div className="flex gap-x-4 py-2">
                                 <div className="min-w-0 flex-auto">
                                     <p className="text-sm font-semibold leading-6 text-gray-900">Imagen</p>
                                 </div>
-                                <div className="min-w-0 flex-auto">
+                                <div className="min-w-0 flex-auto pr-10 w-20">
                                     <p className="text-sm font-semibold leading-6 text-gray-900">Nombre</p>
                                 </div>
                                 <div className="min-w-0 flex-auto">
@@ -54,8 +54,8 @@ export function Prods(props) {
                                 </div>
                             </div>
                             {products.map((product) => (
-                                <div className="flex py-2 border-t border-black ">
-                                    <div className="min-w-0 flex-auto">
+                                <div className="flex py-2 border-t border-black items-center">
+                                    <div className="min-w-0 flex-inital">
                                         <img
                                             src={product.img}
                                             alt=""
@@ -63,23 +63,27 @@ export function Prods(props) {
                                         />
                                     </div>
 
-                                    <div className="min-w-0 flex-auto">
-                                        <p className="text-sm leading-6 text-gray-900" style={{ wordWrap: 'break-word' }}>{product.nombre}</p>
+                                    <div className=" flex-auto pl-10">
+                                        <p className="text-sm leading-6 w-24 text-gray-900" style={{ wordWrap: 'break-word' }}>{product.nombre}</p>
                                     </div>
-                                    <div className="min-w-0 flex-auto">
-                                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">{product.descripcion}</p>
+                                    <div className=" flex-auto">
+                                        <p className="text-sm leading-6 w-32 text-gray-500">{product.descripcion}</p>
                                     </div>
-                                    <div className="min-w-0 flex-auto">
+                                    <div className="flex-auto">
                                         <p className="text-sm leading-6 text-gray-900">{product.tamaño}</p>
                                     </div>
-                                    <div className="min-w-0 flex-auto">
-                                        <p className="text-sm leading-6 text-gray-900">{product.precio}</p>
+                                    <div className="flex-auto ">
+                                        <p className="text-sm leading-6 text-gray-900 ">{product.precio}</p>
                                     </div>
-                                    <div className="flex">
+                                    <div className="flex-auto">
                                         <p className={`text-sm leading-6 ${product.disponibilidad === 1 ? 'text-green-500' : 'text-red-500'}`}>{product.disponibilidad === 1 ? 'DISPONIBLE' : 'NO DISPONIBLE'}</p>
                                     </div>
-                                    <BiEditAlt onClick={handleUpdateClick}/>
-                                    <BiTrash/>
+                                    <div className="flex-end">
+                                        <div className="flex flex-col items-center space-y-2">
+                                            <BiEditAlt onClick={handleUpdateClick} className="text-4xl rounded-md bg-gray-200 hover:bg-gray-300 p-2" />
+                                            <BiTrash className="text-4xl rounded-md bg-gray-200 hover:bg-gray-300 p-2" />
+                                        </div>
+                                    </div>
                                 </div>
 
                             ))}
@@ -157,7 +161,7 @@ export function Prods(props) {
                                     <select
                                         id="stock"
                                         className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                                       /*  value={""} */
+                                        /*  value={""} */
                                         onChange={""}
                                     >
                                         <option value="no">No</option>
@@ -203,120 +207,120 @@ export function Prods(props) {
 
                 )
                 }
-                {state === "actualizar" &&(
-                     <div>Actualiza los datos <button onClick={handleBackClick} className="ml-10 hover:bg-custom-primary text-base border-2 px-4 py-2 rounded-lg"> Volver</button>
-                     <form className="grid grid-cols-6 gap-4">
-                         <div className="col-span-3">
-                             <label htmlFor="nombre" className="block text-xs font-medium text-gray-700">
-                                 Nombre:
-                                 <input
-                                     type="text"
-                                     id="nombre"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
+                {state === "actualizar" && (
+                    <div>Actualiza los datos <button onClick={handleBackClick} className="ml-10 hover:bg-custom-primary text-base border-2 px-4 py-2 rounded-lg"> Volver</button>
+                        <form className="grid grid-cols-6 gap-4">
+                            <div className="col-span-3">
+                                <label htmlFor="nombre" className="block text-xs font-medium text-gray-700">
+                                    Nombre:
+                                    <input
+                                        type="text"
+                                        id="nombre"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
 
-                         <div className="col-span-3">
-                             <label htmlFor="descripcion" className="block text-xs font-medium text-gray-700">
-                                 Descripcion:
-                                 <input
-                                     type="text"
-                                     id="descripcion"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
-                         <div className="col-span-6">
-                             <label htmlFor="precio" className="block text-xs font-medium text-gray-700">
-                                 Precio:
-                                 <input
-                                     type="number"
-                                     id="precio"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
-                         <div className="col-span-6">
-                             <label htmlFor="tamaño" className="block text-xs font-medium text-gray-700">
-                                 tamaño:
-                                 <input
-                                     type="text"
-                                     id="tamaño"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
+                            <div className="col-span-3">
+                                <label htmlFor="descripcion" className="block text-xs font-medium text-gray-700">
+                                    Descripcion:
+                                    <input
+                                        type="text"
+                                        id="descripcion"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
+                            <div className="col-span-6">
+                                <label htmlFor="precio" className="block text-xs font-medium text-gray-700">
+                                    Precio:
+                                    <input
+                                        type="number"
+                                        id="precio"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
+                            <div className="col-span-6">
+                                <label htmlFor="tamaño" className="block text-xs font-medium text-gray-700">
+                                    tamaño:
+                                    <input
+                                        type="text"
+                                        id="tamaño"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
 
-                         <div className="col-span-6">
-                             <label htmlFor="disponibilidad" className="block text-xs font-medium text-gray-700">
-                                 Disponibilidad:
-                                 <input
-                                     type="number"
-                                     id="disponibilidad"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
-                         <div className="col-span-6">
-                             <label htmlFor="stock" className="block text-xs font-medium text-gray-700">
-                                 Stock:
-                                 <select
-                                     id="stock"
-                                     className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                                    /*  value={""} */
-                                     onChange={""}
-                                 >
-                                     <option value="no">No</option>
-                                     <option value="yes">Sí</option>
-                                 </select>
-                             </label>
-                         </div>
-                         <div className="col-span-6">
-                             <label htmlFor="Cantidad Stock" className="block text-xs font-medium text-gray-700">
-                                 Cantidad Stock:
-                                 <input
-                                     type="number"
-                                     id="can_stock"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
-                         <div className="col-span-6">
-                             <label htmlFor="img" className="block text-xs font-medium text-gray-700">
-                                 Imagen:
-                                 <input
-                                     type="text"
-                                     id="img"
-                                     /* value={""} */
-                                     onChange={""}
-                                     className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
-                                 />
-                             </label>
-                         </div>
+                            <div className="col-span-6">
+                                <label htmlFor="disponibilidad" className="block text-xs font-medium text-gray-700">
+                                    Disponibilidad:
+                                    <input
+                                        type="number"
+                                        id="disponibilidad"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
+                            <div className="col-span-6">
+                                <label htmlFor="stock" className="block text-xs font-medium text-gray-700">
+                                    Stock:
+                                    <select
+                                        id="stock"
+                                        className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+                                        /*  value={""} */
+                                        onChange={""}
+                                    >
+                                        <option value="no">No</option>
+                                        <option value="yes">Sí</option>
+                                    </select>
+                                </label>
+                            </div>
+                            <div className="col-span-6">
+                                <label htmlFor="Cantidad Stock" className="block text-xs font-medium text-gray-700">
+                                    Cantidad Stock:
+                                    <input
+                                        type="number"
+                                        id="can_stock"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
+                            <div className="col-span-6">
+                                <label htmlFor="img" className="block text-xs font-medium text-gray-700">
+                                    Imagen:
+                                    <input
+                                        type="text"
+                                        id="img"
+                                        /* value={""} */
+                                        onChange={""}
+                                        className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                    />
+                                </label>
+                            </div>
 
-                         <div className="col-span-6">
-                             <button
-                                 className="block w-full rounded-md bg-custom-secundary font-bunya-bold p-2.5 text-sm text-white transition hover:shadow-lg"
-                                 onClick={""}
-                             >
-                                 Agregar
-                             </button>
-                         </div>
-                     </form>
-                 </div>
+                            <div className="col-span-6">
+                                <button
+                                    className="block w-full rounded-md bg-custom-secundary font-bunya-bold p-2.5 text-sm text-white transition hover:shadow-lg"
+                                    onClick={""}
+                                >
+                                    Agregar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 )}
 
             </div>
