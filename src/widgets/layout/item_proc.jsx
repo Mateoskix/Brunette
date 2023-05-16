c15bbe25fbb74b4e8af87ea299e50a30a6327813
SamuelSanta <samuelsanta2004@gmail.com>
2023-05-16 00:24:54 -0500

    mensaje de alerta al dar boton eliminar en admin

--- src/widgets/layout/item_proc.jsx
+++ src/widgets/layout/item_proc.jsx
@@ -23,6 +23,9 @@ export function Prods(props) {
     const handleBackClick = () => {
         setState("productos");
     };
+    const handleEliminarClick = () => {
+        setState("eliminar");
+    };
     const handleUpdateClick = (product) => {
         setNombre(product.nombre);
         setDescripcion(product.descripcion);
@@ -60,32 +63,32 @@ export function Prods(props) {
     };
     const actualizar = () => {
         const productoActualizado = {
-          nombre,
-          descripcion,
-          tamaño,
-          precio,
-          disponibilidad,
-          stock,
-          img,
+            nombre,
+            descripcion,
+            tamaño,
+            precio,
+            disponibilidad,
+            stock,
+            img,
         };
-      
+
         fetch(`http://localhost:3000/productos/${nombre}`, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ producto: productoActualizado })
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ producto: productoActualizado })
         })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data);
-            console.log("actualización completa")
-          })
-          .catch(error => {
-            console.error(error);
-            console.log("actualización falló")
-          });
-      };
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                console.log("actualización completa")
+            })
+            .catch(error => {
+                console.error(error);
+                console.log("actualización falló")
+            });
+    };
     return (
         <div className="bg-white">
             <div className="mx-20 ">
@@ -130,7 +133,6 @@ export function Prods(props) {
                                             className="h-28 w-28 flex-none bg-gray-50"
                                         />
                                     </div>
-
                                     <div className=" flex-auto pl-10">
                                         <p className="text-sm leading-6 w-24 text-gray-900" style={{ wordWrap: 'break-word' }}>{product.nombre}</p>
                                     </div>
@@ -149,7 +151,7 @@ export function Prods(props) {
                                     <div className="flex-end">
                                         <div className="flex flex-col items-center space-y-2">
                                             <BiEditAlt onClick={handleUpdateClick} className="text-4xl rounded-md bg-gray-200 hover:bg-gray-300 p-2" />
-                                            <BiTrash className="text-4xl rounded-md bg-gray-200 hover:bg-gray-300 p-2" />
+                                            <BiTrash onClick={handleEliminarClick} className="text-4xl rounded-md bg-gray-200 hover:bg-gray-300 p-2" />
                                         </div>
                                     </div>
                                 </div>
@@ -157,6 +159,39 @@ export function Prods(props) {
                             ))}
                         </div>
                     </div>
+                )}
+                {state === "eliminar" && (
+                    <div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+
+                        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+                        <div class="fixed inset-0 z-10 overflow-y-auto">
+                            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                                    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                        <div class="sm:flex sm:items-start">
+                                            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                                <svg class="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+                                                </svg>
+                                            </div>
+                                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                                                <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">¿Estas seguro en eliminar el producto seleccionado?</h3>
+                                                <div class="mt-2">
+                                                    <p class="text-sm text-gray-500">Estás a punto de eliminar el producto seleccionado de forma permanente. Esta acción no se puede deshacer y toda la información asociada al producto se perderá. Asegúrate de que deseas eliminar este producto antes de continuar.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                        <button  type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
+                                        <button onClick={handleBackClick} type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 )}
                 {state === "agregar" && (
                     <div>Ingresa los datos <button onClick={handleBackClick} className="ml-10 hover:bg-custom-primary text-base border-2 px-4 py-2 rounded-lg"> Volver</button>
@@ -345,7 +380,7 @@ export function Prods(props) {
                                     <select
                                         id="stock"
                                         className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
-                                        
+
                                     >
                                         <option value="no">No</option>
                                         <option value="yes">Sí</option>
