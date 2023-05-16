f5e4b6ca37e9b4dba20b7aefeca0749ca2728707
SamuelSanta <samuelsanta2004@gmail.com>
2023-05-16 00:40:33 -0500

    intento eliminar

--- src/widgets/layout/item_proc.jsx
+++ src/widgets/layout/item_proc.jsx
@@ -6,7 +6,6 @@ import { BiTrash, BiEditAlt } from 'react-icons/bi';
 
 export function Prods(props) {
     const { products } = props;
-
     const [nombre, setNombre] = useState('');
     const [descripcion, setDescripcion] = useState('');
     const [tamaño, setTamaño] = useState('');
@@ -36,6 +35,24 @@ export function Prods(props) {
         setImg(product.img);
         setState("actualizar");
     };
+
+    const eliminar = (product) => {
+        fetch(`http://localhost:3000/productos/${product.ID_Producto}`, {
+          method: 'DELETE',
+        })
+          .then(response => {
+            if (response.ok) {
+              console.log('Producto eliminado');
+            } else {
+              console.error('Error al eliminar el producto');
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+          handleBackClick();
+      };
+
     const agregar = () => {
         const producto = {
             nombre,
@@ -184,8 +201,8 @@ export function Prods(props) {
                                         </div>
                                     </div>
                                     <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                        <button  type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Deactivate</button>
-                                        <button onClick={handleBackClick} type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                                        <button onClick={eliminar} type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto">Eliminar</button>
+                                        <button onClick={handleBackClick} type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancelar</button>
                                     </div>
                                 </div>
                             </div>
